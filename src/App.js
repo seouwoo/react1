038b1e7af68a7b7e75cf85d2f6682db2a3ba1133
seou <duswo822@gmail.com>
2024-06-11 14:23:45 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Box from "./components/Box";
+
+function App() {
+  return (
+    <>
+      <Box name="놀부" num={1} />
+      <Box name="흥부" num={2} />
+      <Box name="제비" num={3} />
+    </>
+  );
+}
+
+export default App;
